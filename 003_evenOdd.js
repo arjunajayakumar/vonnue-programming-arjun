d04b903437739aef0a3037755f57f0887d880936
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-11 13:36:15 +0530

    program to find odd or even number

--- 003_evenOdd.js
+++
@@ -1,8 +0,0 @@
-var num = prompt ("Enter the Number");
-if ((num % 2) ===0) {
-
-console.log ("The number is Even")
-}
-else {
-console.log("The number is even")
-}
---
+++ 003_odd_Or_Even_program.js
@@ -0,0 +1,8 @@
+const number = prompt("Enter the Number");
+if ((number % 2) === 0) {
+
+    console.log("The number is Even")
+}
+else {
+    console.log("The number is even")
+}
