82b02902e318621780076883801a3d84efe7f33d
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-13 11:12:49 +0530

    decalred both the vaiables as let

--- 006_sumOfNumbersArray.js
+++ 006_sumOfNumbersArray.js
@@ -4,14 +4,14 @@ const readline = require('readline-sync');
 const sumofArray = (elements) => {
     let numberArray = elements.split(",")
     let arrayLength = numberArray.length
-    sum = 0;
-    for (i = 0; i < arrayLength; i++) {
+    let sum = 0;
+    for (let i = 0; i < arrayLength; i++) {
         sum = (parseInt(numberArray[i]) + parseInt(sum));
     }
     console.log("The sum is:", sum);
 }
 
-let elements = readline.question('enter the numbers seperated by comma ');
+let elements = readline.question('enter the numbers seperated by comma : ');
 sumofArray(elements);
 
 
