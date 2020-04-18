8c659ee1554cb6ad751d170dd83b1dec9cec0417
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-19 00:05:24 +0530

    program to print sum and product by user's choice

---
+++ 010_sumAndMultiplyByN.js
@@ -0,0 +1,35 @@
+/* 10. Write a program that asks the user for a number n and gives them the possibility to choose between 
+computing the sum and computing the product of 1,…,n. */
+
+const operation = (choice) => {
+	if (choice == "m") {
+		MultiplyByN(number);
+	} else {
+		sumByN(number);
+	}
+}
+// Sum
+const sumByN = (number, s) => {
+	let i = 1;
+	let sum = 0
+	while (i <= number) {
+		sum = (sum + i);
+		i++;
+	}
+	console.log("Sum : ", sum);
+}
+// multiply
+const MultiplyByN = (number, m) => {
+	let i = 1;
+	let product = 1;
+	while (i <= number) {
+		product = (product * i);
+		i++;
+	}
+	console.log("Product : ", product);
+}
+
+let number = 11;
+const choice = "m";
+operation(choice);
+
