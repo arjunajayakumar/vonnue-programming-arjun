2b8be22d5871461b7c8b82f3eb9bef5f5785444b
Arjun Ajayakumar <39964358+arjune64@users.noreply.github.com>
2020-04-19 16:13:28 +0530

    Program to multiply array elements with a constant  (#11)
    * progrm to multiply each elelemnt with a constant
    
    * Program to multiply array elements by a constant
    
    * passed constant in to the function

---
+++ 007_multiplyArrayElement.js
@@ -0,0 +1,14 @@
+// 7. Multiply each element of an array with a constant and print the result
+
+const arrayMultiply = (elements, constant) => {
+    let arrayLength = elements.length;
+    let product = [];
+    for (let i = 0; i < arrayLength; i++) {
+        product[i] = parseInt(elements[i]) * constant;
+    }
+    console.log(product);
+}
+
+let elements = [1, 2, 5, 8, 9, 4, 6];
+const constant = 5;
+arrayMultiply(elements, constant);
