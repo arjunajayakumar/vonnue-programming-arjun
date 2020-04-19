3ce74eece54d2ee9f7a2f05ee3446fa1165adcf4
Arjun Ajayakumar <39964358+arjune64@users.noreply.github.com>
2020-04-19 16:03:16 +0530

    program to check whether the entered number is odd or even (#5)
    * program to check whether the entered number is odd or even
    
    * rewrote program with function
    
    * 'else' included in the curly bracket
    
    * removed semicolon from line no:12

---
+++ 013_odd_Or_Even_program.js
@@ -0,0 +1,13 @@
+// Program to find Even and Odd number
+const readline = require("readline-sync");
+
+const oddEven = (num) => {
+    if ((num % 2) === 0) {
+        console.log("The number is Even");
+    } else {
+        console.log("The number is odd");
+    }
+}
+
+const num = readline.question('Enter the number : ');
+oddEven(num);
