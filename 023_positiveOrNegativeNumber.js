d98149f4b94b6ca4eca76b31baf250624996591a
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-22 01:19:01 +0530

    This commit will implement a program to check a number is positive or negative

---
+++ 023_positiveOrNegativeNumber.js
@@ -0,0 +1,8 @@
+// 23. Write a program to check a number is positive or negative
+const positiveOrNegative = () => {
+  let number = '-8';
+  sign = Math.sign(number);
+  console.log(sign);
+}
+
+positiveOrNegative();
