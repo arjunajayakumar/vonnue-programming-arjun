d2f026a018cf4c864a924f544e9b5f8c9951c5cf
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-22 01:21:45 +0530

    Revert "This commit will implement a program to check a number is positive or negative"
    This reverts commit d98149f4b94b6ca4eca76b31baf250624996591a.

--- 023_positiveOrNegativeNumber.js
+++
@@ -1,8 +0,0 @@
-// 23. Write a program to check a number is positive or negative
-const positiveOrNegative = () => {
-  let number = '-8';
-  sign = Math.sign(number);
-  console.log(sign);
-}
-
-positiveOrNegative();
