4c616ea734ad78b0d139255e37f85a23853f0d58
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-22 12:45:49 +0530

    This commit will chnage the program in the correct way to print the next 20 leap years

--- 018_LeapYear.js
+++ 018_LeapYear.js
@@ -1,12 +1,16 @@
 // 18. Write a program that prints the next 20 leap years.
 
-const leapYear = (year) => {
-    for (let i = 2021; i <= year; i++) {
-        if ((i % 4) === 0) {
-            console.log(i);
-        }
+const leapYear = () => {
+  let count = 0;
+  for (let i = 2021; i > count; i++) {
+    if ((i % 4) === 0) {
+      console.log(i);
+      count++;
     }
+    if (count === 20) {
+      break;
+    }
+  }
 }
 
-let year = 2100;
-leapYear(year);
+leapYear();
