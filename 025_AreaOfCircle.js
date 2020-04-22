30417b2365542e05d61a826d0e3b027504d638ce
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-22 22:55:25 +0530

    This commit will modify the function call by passing arguments

--- 025_AreaOfCircle.js
+++ 025_AreaOfCircle.js
@@ -1,9 +1,8 @@
 // 25. Find Area of a circle
-
-const circleArea = () => {
-  let radius = 4;
+const circleArea = (radius) => {
   let area = Math.PI * (radius * radius);
   console.log('Area :', area);
 }
 
-circleArea();
+let radius = 4;
+circleArea(radius);
