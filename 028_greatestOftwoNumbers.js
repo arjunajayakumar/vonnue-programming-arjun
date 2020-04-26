b5bb5fe4b78b63d0a92cea9f6472d5703b794a34
Arjun Ajayakumar <arjune64@gmail.com>
2020-04-26 22:34:17 +0530

    This commit will implement a program to find the greatest of two numbers

---
+++ 028_greatestOftwoNumbers.js
@@ -0,0 +1,11 @@
+// 28. Greatest of two numbers
+const greatest = (firstNumber, secondNumber) => {
+  if (firstNumber > secondNumber) {
+    return firstNumber;
+  } else {
+    return secondNumber;
+  }
+}
+
+console.log(greatest(30, 78));
+
